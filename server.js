ba1e80d9018f0ad1a538deb1427e292d0aa337f9
sagepages-okta <134405057+sagepages-okta@users.noreply.github.com>
2023-06-29 00:30:15 -0400

    Initial project upload

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import * as Sentry from "@sentry/node";
+
+// or using CommonJS
+// const express = require('express');
+// const Sentry = require('@sentry/node');
+
+const app = express();
+
+Sentry.init({
+  dsn: "https://f6b875eca718446ca451adac5a325340@o27592.ingest.sentry.io/4505439797248000",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context, so that all
+// transactions/spans/breadcrumbs are isolated across requests
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// All controllers should live here
+app.get("/", function rootHandler(req, res) {
+  res.end("Hello world!");
+});
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
+app.listen(3000);
